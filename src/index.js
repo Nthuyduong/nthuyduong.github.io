c602f41a19857fae75246e56a91ddee80ef376f6
giangtx13 <giangtx13@gmail.com>
2024-05-05 09:33:51 +0700

    giang test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "@fortawesome/fontawesome-svg-core"
 import "@fortawesome/react-fontawesome"
 import "./fonts.scss";
 import './styles/tailwind.css'
-
+// test
 const container = document.getElementById("root");
 const root = createRoot(container);
 
