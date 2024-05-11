3514bbe717f56959e4b558fe54730b4c81e0a8c6
Nthuyduong <nguyenthuyduong15113@gmail.com>
2024-05-11 21:43:59 +0700

    header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from "react";
-import {ROUTER} from "../../utils/constants";
+import { ROUTER } from "../../utils/constants";
 import { useHistory, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
@@ -71,41 +71,63 @@ const Header = () => {
             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
               {/*Mobile menu button*/}
               <button type="button"
-                      className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                      aria-controls="mobile-menu" aria-expanded="false">
+                className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                aria-controls="mobile-menu" aria-expanded="false">
                 <span className="absolute -inset-0.5"></span>
                 <span className="sr-only">Open main menu</span>
 
-                  {/*Icon when menu is closed.*/}
+                {/*Icon when menu is closed.*/}
 
-                  {/*Menu open: "hidden", Menu closed: "block"*/}
+                {/*Menu open: "hidden", Menu closed: "block"*/}
 
                 <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                     aria-hidden="true">
+                  aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
 
-                  {/*Icon when menu is open.*/}
+                {/*Icon when menu is open.*/}
 
-                  {/*Menu open: "block", Menu closed: "hidden"*/}
+                {/*Menu open: "block", Menu closed: "hidden"*/}
 
                 <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                     aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
+                  aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                 </svg>
               </button>
             </div>
-            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="my-header flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
               <div className="hidden w-full md:block">
                 <div className="flex space-x-4 justify-center w-full">
                   {/*Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white"*/}
-                  <Link to="/" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-4 text-sm font-medium" aria-current="page">HOME</Link>
+                  <Link to="/" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-4 text-sm font-medium" aria-current="page">Home</Link>
                   <Link to={ROUTER.ABOUT} className="text-gray-300 hover:text-white rounded-md px-3 py-4 text-sm font-medium">
-                    ABOUT
+                    About
                   </Link>
-                  <Link to={ROUTER.DESIGNS} className="text-gray-300 hover:text-white rounded-md px-3 py-4 text-sm font-medium">DESIGNS</Link>
-                  <Link to={ROUTER.CONTACT} className="text-gray-300 hover:text-white rounded-md px-3 py-4 text-sm font-medium">CONTACT</Link>
+                  {/* Dropdown menu */}
+                  <div className="header-nav-item relative header-navdrop">
+                    <div className="navdrop-title z-50 text-gray-300 hover:text-gray rounded-md pr-3 py-4">
+                      <Link to={ROUTER.DESIGNS} className="text-sm font-medium dark:text-white flex nav-link"><span className="nav-text">Projects</span>
+                        
+                      </Link>
+                    </div>
+                    <div className="navdrop-inner absolute left-30 w-48 py-1 border-solid border-x border-b border-ccc dark:border-999">
+                      <div className="sub-menu block px-4 py-2 text-sm text-gray-700 dark:!text-white">
+                        <Link href={'/sub-des/5'} className="body_text nav-link">Skin concerns</Link>
+                      </div>
+                      <div className="sub-menu block px-4 py-2 text-sm text-gray-700 dark:!text-white">
+                        <Link href={'/sub-des/6'} className="body_text nav-link">Tips & advices</Link>
+                      </div>
+                      <div className="sub-menu block px-4 py-2 text-sm text-gray-700 dark:!text-white">
+                        <Link href={'/sub-des/7'} className="body_text nav-link">Hair care</Link>
+                      </div>
+                      <div className="sub-menu block px-4 py-2 text-sm text-gray-700 dark:!text-white">
+                        <Link href={'/sub-des/8'} className="body_text nav-link">Mental health</Link>
+                      </div>
+                    </div>
+                  </div>
+
+                  <Link to={ROUTER.CONTACT} className="text-gray-300 hover:text-white rounded-md px-3 py-4 text-sm font-medium">Contact</Link>
                 </div>
               </div>
             </div>
@@ -117,13 +139,13 @@ const Header = () => {
           <div className="space-y-1 px-2 pb-3 pt-2">
             {/*Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white"*/}
             <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
-               aria-current="page">Dashboard</a>
+              aria-current="page">Dashboard</a>
             <a href="#"
-               className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
             <a href="#"
-               className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
             <a href="#"
-               className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
           </div>
         </div>
       </nav>
