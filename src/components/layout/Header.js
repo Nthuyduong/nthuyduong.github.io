1485114af30fccfe9df58a14cb72ca27baafcab4
Nthuyduong <nguyenthuyduong15113@gmail.com>
2024-06-06 11:56:46 +0700

    commit

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -24,20 +24,20 @@ const Header = () => {
     }
   }, [])
 
-  
+
   let prevScrollpos = 0;
 
   const controlNavbar = () => {
     var currentScrollPos = window.pageYOffset;
     if (headerRef.current) {
       if (prevScrollpos > currentScrollPos) {
-          headerRef.current.style.top = "0";
-          headerMobileRef.current.style.top = "0";
+        headerRef.current.style.top = "0";
+        headerMobileRef.current.style.top = "0";
       } else {
-          headerRef.current.style.top = "-75px";
-          headerMobileRef.current.style.top = "-105px";
-          setOpenMobileMenu(false);
-          setOpenNavDropdown(false);
+        headerRef.current.style.top = "-75px";
+        headerMobileRef.current.style.top = "-105px";
+        setOpenMobileMenu(false);
+        setOpenNavDropdown(false);
       }
       prevScrollpos = currentScrollPos;
     }
@@ -45,7 +45,7 @@ const Header = () => {
 
   return (
     <>
-      <div ref={headerMobileRef} className={`sticky header-mobile inset-y-0 left-0 flex items-center sm:hidden flex-col w-full border-b border-ccc border-solid ${openMobileMenu? 'header-mobile-open' : ''}`}>
+      <div ref={headerMobileRef} className={`sticky header-mobile inset-y-0 left-0 flex items-center sm:hidden flex-col w-full border-b border-ccc border-solid ${openMobileMenu ? 'header-mobile-open' : ''}`}>
         {/*Mobile menu button*/}
         <div
           className="mobile-menu-btn w-full relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
@@ -53,21 +53,21 @@ const Header = () => {
         >
           <div className="flex gap-1">
             Menu
-            <img className='btn-header-mobile' src="/images/arrow-down.png" alt=""/>
+            <img className='btn-header-mobile' src="/images/arrow-down.png" alt="" />
           </div>
         </div>
         <div className="w-full flex flex-col header-mobile-menu py-4">
           <Link to="/" className="cursor-text-wrp text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 text-4xl font-medium" aria-current="page">Home</Link>
           <Link to={ROUTER.ABOUT} className="cursor-text-wrp text-gray-300 hover:text-white rounded-md px-3 text-4xl font-medium">About</Link>
           <div className="header-nav-item relative header-mobile-navdrop px-3">
-            <div 
+            <div
               className="navdrop-title z-50 text-gray-300 hover:text-gray rounded-md pr-3 flex w-full"
               onClick={() => setOpenNavDropdown(!openNavDropdown)}
             >
               <Link to={ROUTER.DESIGNS} className="text-4xl font-medium flex nav-link"><span className="nav-text">Projects</span></Link>
               <span className="w-full">(04)</span>
             </div>
-            <div className={`navdrop-mobile-inner left-30 w-48 ${openNavDropdown ? 'navdrop-mobile-inner-open': ''}`}>
+            <div className={`navdrop-mobile-inner left-30 w-48 ${openNavDropdown ? 'navdrop-mobile-inner-open' : ''}`}>
               <div className="sub-menu block px-4 text-xl text-gray-700">
                 <Link to={ROUTER.AGURI} className="nav-link">Jewelry Website</Link>
               </div>
@@ -90,14 +90,16 @@ const Header = () => {
         <div className="hidden w-full md:block">
           <div className="flex space-x-4 justify-center w-full">
             {/*Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white"*/}
-            <Link to="/" className="cursor-text-wrp hover:bg-gray-700 rounded-md px-3 py-4 text-sm " aria-current="page">Home</Link>
-            <Link to={ROUTER.ABOUT} className="navdrop-title cursor-text-wrp rounded-md px-3 py-4 text-sm ">
-              About
+            <Link to="/" className="cursor-text-wrp hover:bg-gray-700 rounded-md px-3 py-4 text-sm scroll-text" data-replace="HOME" aria-current="page">HOME</Link>
+            <Link to={ROUTER.ABOUT} className="navdrop-title cursor-text-wrp rounded-md px-3 py-4 text-sm scroll-text" data-replace="ABOUT">
+              ABOUT
             </Link>
             {/* Dropdown menu */}
             <div className="header-nav-item relative header-navdrop">
               <div className="navdrop-title z-50 rounded-md pr-3 py-4">
-                <Link to={ROUTER.DESIGNS} className="text-sm flex nav-link"><span className="nav-text">Projects</span></Link>
+                <Link to={ROUTER.DESIGNS} className="text-sm flex nav-link scroll-text" data-replace="PROJECTS">
+                  <span className="nav-text">PROJECTS</span>
+                </Link>
               </div>
               <div className="navdrop-inner absolute left-30 w-48 py-1 border-solid border-x border-b border-ccc">
                 <div className="sub-menu block px-4 py-2 text-sm">
