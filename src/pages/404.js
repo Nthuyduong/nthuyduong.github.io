e4fdedc02feaab7abf332b6ed1e2870e393c52e0
Nthuyduong <nguyenthuyduong15113@gmail.com>
2024-06-07 17:28:12 +0700

    commit

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { ROUTER } from "../utils/constants";
+import Slider from "../components/common/slider";
+
+const NotFound = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default NotFound;
+
