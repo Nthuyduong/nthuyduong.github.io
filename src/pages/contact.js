3e512e3c15fab3e4c7f0580f4d3762084237ae5b
Nthuyduong <nguyenthuyduong15113@gmail.com>
2024-06-10 08:59:45 +0700

    add link contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -70,117 +70,123 @@ const Contact = () => {
   }
 
   return (
-    <div className="px-3 md:px-0 contact-page mt-4 md:mt-7 md:mb-9 mb-4">
-      <div className="md:grid grid-cols-12 gap-4 mb-2 md:mb-5">
-        <div className="col-span-1"></div>
-        <div className="col-span-11">
-          <div className="heading_1 cursor-text-wrp">Get In Touch</div>
-        </div>
-      </div>
-      <div className="md:grid grid-cols-12 gap-4">
-        <div className="col-span-1"></div>
-        <div className="col-span-3 relative h-fit">
-          <div className="body_text cursor-text-wrp md:mb-0 mb-5">
-            “I welcome any potential recruiters to contact me regarding job opportunities that match my skillset and interests. I am excited to explore new opportunities and connect with professionals in my field”
+    <div className="section">
+      <div className="px-3 md:px-0 contact-page mt-4 md:mt-7 md:mb-9 mb-4">
+        <div className="md:grid grid-cols-12 gap-4 mb-2 md:mb-5">
+          <div className="col-span-1"></div>
+          <div className="col-span-11">
+            <div className="heading_1 cursor-text-wrp">Get In Touch</div>
           </div>
-          <div className="hidden md:grid grid-cols-2 gap-4 mt-8">
-            <div className="cols-span-1">
-              <img className="w-full" src="./images/contact/contactpage.png" alt="logo" loading="lazy" />
+        </div>
+        <div className="md:grid grid-cols-12 gap-4">
+          <div className="col-span-1"></div>
+          <div className="col-span-4 relative h-fit">
+            <div className="body_text cursor-text-wrp md:mb-0 mb-5">
+              “I welcome any potential recruiters to contact me regarding job opportunities that match my skillset and interests. I am excited to explore new opportunities in my field”
             </div>
-            <div className="cols-span-1 flex self-end">
-              <div className="w-full text-right">
-                <div className="small_text mb-1 cursor-text-wrp">
-                  Behance
+            <div className="hidden md:grid grid-cols-2 gap-4 mt-8">
+              <div className="cols-span-1">
+                <img className="w-full" src="./images/contact/contactpage.png" alt="logo" loading="lazy" />
+              </div>
+              <div className="cols-span-1 flex self-end">
+                <div className="w-full text-right">
+                  <div className="small_text mb-1 cursor-text-wrp">
+                    <a className="" href="https://www.behance.net/duongthuy13" target="_blank">
+                      Behance
+                    </a>
+                  </div>
+                  <div className="small_text mb-1 cursor-text-wrp">
+                    Facebook
+                  </div>
+                  <div className="small_text cursor-text-wrp">
+                    <a href="https://www.instagram.com/nth_d9">
+                      Instagram
+                    </a>
+                  </div>
                 </div>
-                <div className="small_text mb-1 cursor-text-wrp">
-                  Facebook
-                </div>
-                <div className="small_text cursor-text-wrp">
-                  Instagram
+              </div>
+            </div>
+            <div className="roll-animation contact-roll absolute md:block hidden">
+              <div className="roll">
+                <div className="circle">
+                  <img className="h-[200px] w-full" src="./images/contact/contact-roll.svg" alt="logo" loading="lazy" />
                 </div>
               </div>
             </div>
           </div>
-          <div className="roll-animation contact-roll absolute md:block hidden">
-            <div className="roll">
-              <div className="circle">
-                <img className="h-[200px] w-full" src="./images/contact/contact-roll.svg" alt="logo" loading="lazy" />
+          <div className="col-span-1"></div>
+          <div className="col-span-6">
+            {/* <div className="heading_6 mb-5 cursor-text-wrp">Send me a message</div> */}
+            <div className="md:grid grid-cols-2 gap-4">
+              <div className="md:pb-0 mb-4 col-span-1 grid contact_field relative">
+                <label className="mb-1 cursor-text-wrp">Your Name *</label>
+                <input
+                  className={`input-form ${error?.name ? 'border-[red]' : 'border-ccc'}`}
+                  type="text"
+                  required
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <span className="focus-border"></span>
+              </div>
+              <div className="md:pb-0 mb-4 col-span-1 grid contact_field relative">
+                <label className="mb-1 cursor-text-wrp">Phone number *</label>
+                <input
+                  className={`input-form ${error?.phone ? 'border-[red]' : 'border-ccc'}`}
+                  type="number"
+                  required
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                />
+                <span className="focus-border"></span>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="col-span-1"></div>
-        <div className="col-span-6">
-          {/* <div className="heading_6 mb-5 cursor-text-wrp">Send me a message</div> */}
-          <div className="md:grid grid-cols-2 gap-4">
-            <div className="md:pb-0 mb-4 col-span-1 grid contact_field relative">
-              <label className="mb-1 cursor-text-wrp">Your Name *</label>
+            <div className="grid my-4 contact_field relative">
+              <label className="mb-1 cursor-text-wrp">Email address *</label>
               <input
-                className={`input-form ${error?.name ? 'border-[red]' : 'border-ccc'}`}
+                className={`input-form ${error?.email ? 'border-[red]' : 'border-ccc'}`}
                 type="text"
                 required
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
               <span className="focus-border"></span>
             </div>
-            <div className="md:pb-0 mb-4 col-span-1 grid contact_field relative">
-              <label className="mb-1 cursor-text-wrp">Phone number *</label>
-              <input
-                className={`input-form ${error?.phone ? 'border-[red]' : 'border-ccc'}`}
-                type="number"
+            <div className="grid contact_field relative">
+              <label className="mb-1 cursor-text-wrp">Message *</label>
+              <textarea
+                className={`input-form ${error?.message ? 'border-[red]' : 'border-ccc'}`}
+                rows="4"
                 required
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
-              />
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              ></textarea>
               <span className="focus-border"></span>
             </div>
-          </div>
-          <div className="grid my-4 contact_field relative">
-            <label className="mb-1 cursor-text-wrp">Email address *</label>
-            <input
-              className={`input-form ${error?.email ? 'border-[red]' : 'border-ccc'}`}
-              type="text"
-              required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-             <span className="focus-border"></span>
-          </div>
-          <div className="grid contact_field relative">
-            <label className="mb-1 cursor-text-wrp">Message *</label>
-            <textarea
-              className={`input-form ${error?.message ? 'border-[red]' : 'border-ccc'}`}
-              rows="4"
-              required
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            ></textarea>
-            <span className="focus-border"></span>
-          </div>
-          <div className="text-[42px] send-msg-btn mt-6">
-            <div className="flex float-right">
-              <button
-                type="submit"
-                className={`relative cursor-pointer cursor-text-wrp flex gap-2 btn-contact ${loading ? 'btn-wrp-loading' : ''} ${success ? 'btn-wrp-success' : ''}`}
-                onClick={handleSubmit}
-                disabled={loading}
-              >
-                <div className="btn-loading">
-                  Sending <div className="w-[75px]"><LoadingMail /></div>
-                </div>
-                <div className="btn-contact-success flex items-center">
-                  Sent successfully
-                </div>
-                <div className="btn-contact-text flex items-center">
-                  Send Message
-                  <img className="h-[63px]" src="./images/icons/arrow-up-right-l.svg" alt="logo" loading="lazy" />
-                </div>
-              </button>
+            <div className="text-[42px] send-msg-btn mt-6">
+              <div className="flex float-right">
+                <button
+                  type="submit"
+                  className={`relative cursor-pointer cursor-text-wrp flex gap-2 btn-contact ${loading ? 'btn-wrp-loading' : ''} ${success ? 'btn-wrp-success' : ''}`}
+                  onClick={handleSubmit}
+                  disabled={loading}
+                >
+                  <div className="btn-loading">
+                    Sending <div className="w-[75px]"><LoadingMail /></div>
+                  </div>
+                  <div className="btn-contact-success flex items-center">
+                    Sent successfully
+                  </div>
+                  <div className="btn-contact-text flex items-center">
+                    Send Message
+                    <img className="h-[63px]" src="./images/icons/arrow-up-right-l.svg" alt="logo" loading="lazy" />
+                  </div>
+                </button>
+              </div>
             </div>
           </div>
+          {/* <div className="col-span-1"></div> */}
         </div>
-        <div className="col-span-1"></div>
       </div>
     </div>
   )
