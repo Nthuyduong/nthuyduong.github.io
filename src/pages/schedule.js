d1666854c7faa02d136b18ba371f8a7360a0a451
Nthuyduong <nguyenthuyduong15113@gmail.com>
2025-03-31 18:20:51 +0700

    schedule img

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -67,7 +67,7 @@ const Schedule = () => {
                     <img className="w-100" src="./images/Pj08_schedule/typography.webp" loading="lazy"/>
                 </div>
                 <div className="project-item md:pb-6 pb-4">
-                    <img className="w-100" src="./images/Pj08_schedule/color-palette.webp" loading="lazy"/>
+                    <img className="w-100" src="./images/Pj08_schedule/Color-palette.webp" loading="lazy"/>
                 </div>
                 <div className="project-item md:pb-6 pb-4">
                     <img className="w-100" src="./images/Pj08_schedule/schedule-mockup.webp" loading="lazy"/>
