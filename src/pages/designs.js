6000e1358fb5dfb4cfbc344ed8db696fe12c8693
Nthuyduong <nguyenthuyduong15113@gmail.com>
2025-04-02 14:37:16 +0700

    commit

--- src/pages/designs.js
+++ src/pages/designs.js
@@ -214,7 +214,7 @@ const Designs = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="lg:mt-6 border-b border-solid border-ccc md:pb-0 pb-5">
+                            <div className="border-b border-solid border-ccc md:pb-0 pb-5">
                                 <div className="design-scale-out md:flex flex-row gap-6">
                                     <div className="md:flex flex-row gap-5 lg:gap-6 w-full py-5">
                                         <div className="heading_3 mb-3 cursor-text-wrp hidden lg:block">03</div>
@@ -468,8 +468,60 @@ const Designs = () => {
                         </div>
                         {/* } */}
                         {/* {activeTab === 'E - COMMERCE (0)' && */}
-                        <div className={`content-tab ${activeTab === 'SHOPIFY (2)' ? 'content-active' : ''}`}>
+                        <div className={`content-tab ${activeTab === 'SHOPIFY & APP (2)' ? 'content-active' : ''}`}>
                             <div className="lg:mt-6 md:pb-0 pb-5">
+                            <div className="border-b border-solid border-ccc md:pb-0 pb-5">
+                                    <div className="design-scale-out md:flex flex-row gap-6">
+                                        <div className="md:flex flex-row gap-5 lg:gap-6 w-full py-5">
+                                            <div className="heading_3 mb-3 cursor-text-wrp hidden lg:block">01</div>
+                                            <img
+                                                className="w-full md:w-[50%] block lg:hidden md:max-w-[214px]"
+                                                src="./images/design/mydes4.png"
+                                                alt="logo"
+                                                loading="lazy"
+                                            />
+                                            <div className="sm:pt-0 pt-4">
+                                                <div className="flex">
+                                                    <div
+                                                        className="mb-1 cursor-text-wrp body_text pr-2 mr-2 border-r border-solid border-999">UX/UI
+                                                        Design
+                                                    </div>
+                                                    <div className="cursor-text-wrp body_text">March 2025</div>
+                                                </div>
+                                                <div>
+                                                    <Link >
+                                                        <div className="heading_3 cursor-text-wrp">Link Shortener Service comming...
+                                                            Website
+                                                        </div>
+                                                    </Link>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                        <div
+                                            className="md:flex flex-row justify-end relative w-[40%] sm:w-[25%] lg:w-full">
+                                            <Link >
+                                                <div className="design-scale-wrp absolute h-full hidden lg:block">
+                                                    <img
+                                                        className="design-scale-img h-full w-auto object-cover"
+                                                        src="./images/design/mydes6.png"
+                                                        alt="logo"
+                                                        loading="lazy"
+                                                    />
+                                                </div>
+                                            </Link>
+                                            <Link className="py-5" >
+                                                <div className="flex items-center">
+                                                    <div className="mr-2">Explore now</div>
+                                                    <div>
+                                                        <img className="" src="./images/icons/arrow.svg" alt="smile"
+                                                            loading="lazy" />
+                                                    </div>
+                                                </div>
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </div>
                                 <div className="border-b border-solid border-ccc md:pb-0 pb-5">
                                     <div className="design-scale-out md:flex flex-row gap-6">
                                         <div className="md:flex flex-row gap-5 lg:gap-6 w-full py-5">
@@ -528,7 +580,7 @@ const Designs = () => {
                                             <div className="heading_3 mb-3 cursor-text-wrp hidden lg:block">02</div>
                                             <img
                                                 className="w-full md:w-[50%] block lg:hidden md:max-w-[214px]"
-                                                src="./images/design/mydes4.png"
+                                                src="./images/design/mydes10.png"
                                                 alt="logo"
                                                 loading="lazy"
                                             />
@@ -555,7 +607,7 @@ const Designs = () => {
                                                 <div className="design-scale-wrp absolute h-full hidden lg:block">
                                                     <img
                                                         className="design-scale-img h-full w-auto object-cover"
-                                                        src="./images/design/mydes6.png"
+                                                        src="./images/design/mydes10.png"
                                                         alt="logo"
                                                         loading="lazy"
                                                     />
---
+++ src/pages/short.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import { ROUTER } from "../utils/constants";
+import Slider from "../components/common/slider";
+import { Link } from "react-router-dom";
+
+const Short = () => {
+    return (
+        <div>
+            <div className="">
+                <div className="container-fluid">
+                    <div className="project-item alio1 md:mb-6 mb-4">
+                        <img className="w-100 w-full" src="./images/test/mockup.webp" loading="lazy" />
+                    </div>
+                    
+                    <div className="project-item md:pb-6 pb-4">
+                        <div className="grid grid-cols-12">
+                            <div className="col-span-12 sm:col-span-6">
+                                <img className="w-100" src="./images/test/deerly.webp" alt="#" loading="lazy" />
+                            </div>
+                            <div className="col-span-12 sm:col-span-6 cake-intro-content flex items-end bg-[#FFFDF8]">
+                                <div className="p-7">
+                                    <div className="heading_4 mb-4">About website</div>
+                                    <div className="body_text">
+                                        Welcome to The Deerly, a carefully curated online store
+                                        designed with both style and functionality in mind. As a UX/UI designer, I’ve focused on creating a seamless and visually appealing shopping experience. Our collection features unique, high-quality products that bring a touch of elegance to your home. From décor to accessories, each item is selected for its craftsmanship and beauty. Explore and shop with ease, and let us help you
+                                        transform your space into something truly special.
+                                    </div>
+                                    {/* <div className="small_text text-right font-italic">
+                                        <a href="https://nthuyduong.github.io/CakeBlog/#/" target="_blank">Comming soon: https://nthuyduong.github.io/CakeBlog/#/</a>
+                                    </div> */}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="md:pb-6 pb-4 text-center">
+                        <div className="mb-2 heading_6">VIEW THE WEBSITE</div>
+                        <a className="text-link" href="https://thedeerly.com/" target="_blank">https://thedeerly.com/</a>
+                    </div>
+                    <div className="project-item md:pb-6 pb-4">
+                        <img className="w-100 w-full" src="./images/test/deerly-mobile.webp" loading="lazy" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Short;
