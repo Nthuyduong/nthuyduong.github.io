07028cc67ec8fdc3a19ca866c1e3104b19765ee5
Nthuyduong <nguyenthuyduong15113@gmail.com>
2025-04-03 09:32:57 +0700

    short link

--- src/pages/short.js
+++ src/pages/short.js
@@ -9,10 +9,10 @@ const Short = () => {
             <div className="">
                 <div className="container-fluid">
                     <div className="project-item alio1 md:mb-6 mb-4">
-                        <img className="w-100 w-full" src="./images/Pj09_shortlink.webp" loading="lazy" />
+                        <img className="w-100 w-full" src="./images/Pj09_shortlink/shortlink.webp" loading="lazy" />
                     </div>
                     
-                    <div className="project-item md:pb-6 pb-4">
+                    {/* <div className="project-item md:pb-6 pb-4">
                         <div className="grid grid-cols-12">
                             <div className="col-span-12 sm:col-span-6">
                                 <img className="w-100" src="./images/test/deerly.webp" alt="#" loading="lazy" />
@@ -25,20 +25,12 @@ const Short = () => {
                                         designed with both style and functionality in mind. As a UX/UI designer, I’ve focused on creating a seamless and visually appealing shopping experience. Our collection features unique, high-quality products that bring a touch of elegance to your home. From décor to accessories, each item is selected for its craftsmanship and beauty. Explore and shop with ease, and let us help you
                                         transform your space into something truly special.
                                     </div>
-                                    {/* <div className="small_text text-right font-italic">
-                                        <a href="https://nthuyduong.github.io/CakeBlog/#/" target="_blank">Comming soon: https://nthuyduong.github.io/CakeBlog/#/</a>
-                                    </div> */}
+                                    
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="md:pb-6 pb-4 text-center">
-                        <div className="mb-2 heading_6">VIEW THE WEBSITE</div>
-                        <a className="text-link" href="https://thedeerly.com/" target="_blank">https://thedeerly.com/</a>
-                    </div>
-                    <div className="project-item md:pb-6 pb-4">
-                        <img className="w-100 w-full" src="./images/test/deerly-mobile.webp" loading="lazy" />
-                    </div>
+                    </div> */}
+                    
                 </div>
             </div>
         </div>
