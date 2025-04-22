fc23310998890bfd3164555853d08b846f3585bf
Nthuyduong <nguyenthuyduong15113@gmail.com>
2025-04-22 14:26:41 +0700

    shopify app

--- src/pages/shopifyapp.js
+++ src/pages/shopifyapp.js
@@ -7,44 +7,44 @@ const Shopifyapp = () => {
     return (
         <div className="cake-page">
             <div className="container-fluid">
-                Shopify app + Extension
-                <div className="project-item md:pb-6 pb-4">
-                    <div className="grid grid-cols-12">
-                        <div className="col-span-12 sm:col-span-6">
-                            content left
-                        </div>
-                        <div className="col-span-12 sm:col-span-6">
-                            <img className="w-100 w-full" src="./images/Pj08_schedule/goal.webp" alt="#"
-                                 loading="eager" height={500} width={500}/>
-                        </div>
-                    </div>
-                </div>
-                <div className="project-item md:pb-6 pb-4">
-                    <div className="grid grid-cols-12">
-                        <div className="col-span-12 sm:col-span-6">
-                            <img className="w-100 w-full" src="./images/Pj08_schedule/feature-l.webp" alt="#"
-                                 loading="lazy"/>
-                        </div>
-                        <div className="col-span-12 sm:col-span-6">
-                            <img className="w-100 w-full" src="./images/Pj08_schedule/feature-r.webp" alt="#"
-                                 loading="eager" height={500} width={500}/>
-                        </div>
-                    </div>
-                </div>
-                <div className="project-item md:pb-6 pb-4">
-                    <img className="w-100" src="./images/Pj08_schedule/schedule02.webp" loading="lazy"/>
-                </div>
-                <div className="project-item md:pb-6 pb-4">
-                    <div className="grid grid-cols-12">
-                        <div className="col-span-12 sm:col-span-6">
-                            <img className="w-100" src="./images/Pj08_schedule/sitemap-l.webp" alt="#" loading="lazy"/>
-                        </div>
-                        <div className="col-span-12 sm:col-span-6">
-                            <img className="w-100 w-full" src="./images/Pj08_schedule/sitemap-r.webp" alt="#"
-                                 loading="eager" height={500} width={500}/>
-                        </div>
-                    </div>
-                </div>
+                Comming Soon!
+                {/*<div className="project-item md:pb-6 pb-4">*/}
+                {/*    <div className="grid grid-cols-12">*/}
+                {/*        <div className="col-span-12 sm:col-span-6">*/}
+                {/*            content left*/}
+                {/*        </div>*/}
+                {/*        <div className="col-span-12 sm:col-span-6">*/}
+                {/*            <img className="w-100 w-full" src="./images/Pj08_schedule/goal.webp" alt="#"*/}
+                {/*                 loading="eager" height={500} width={500}/>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
+                {/*<div className="project-item md:pb-6 pb-4">*/}
+                {/*    <div className="grid grid-cols-12">*/}
+                {/*        <div className="col-span-12 sm:col-span-6">*/}
+                {/*            <img className="w-100 w-full" src="./images/Pj08_schedule/feature-l.webp" alt="#"*/}
+                {/*                 loading="lazy"/>*/}
+                {/*        </div>*/}
+                {/*        <div className="col-span-12 sm:col-span-6">*/}
+                {/*            <img className="w-100 w-full" src="./images/Pj08_schedule/feature-r.webp" alt="#"*/}
+                {/*                 loading="eager" height={500} width={500}/>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
+                {/*<div className="project-item md:pb-6 pb-4">*/}
+                {/*    <img className="w-100" src="./images/Pj08_schedule/schedule02.webp" loading="lazy"/>*/}
+                {/*</div>*/}
+                {/*<div className="project-item md:pb-6 pb-4">*/}
+                {/*    <div className="grid grid-cols-12">*/}
+                {/*        <div className="col-span-12 sm:col-span-6">*/}
+                {/*            <img className="w-100" src="./images/Pj08_schedule/sitemap-l.webp" alt="#" loading="lazy"/>*/}
+                {/*        </div>*/}
+                {/*        <div className="col-span-12 sm:col-span-6">*/}
+                {/*            <img className="w-100 w-full" src="./images/Pj08_schedule/sitemap-r.webp" alt="#"*/}
+                {/*                 loading="eager" height={500} width={500}/>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
             </div>
         </div>
     )
